26a1b58c11ce552a9f3b864914a2535cc511c6ee
Alex Nordin <apnordin@gmail.com>
2021-02-04 10:19:17 -0500

    Added proxy server in package.json inside client, now only getting 500 errors rather than nightmare port errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
-// import ChatBox from "./components/ChatBox";
 import Navbar from "./components/Navbar";
 import Signup from "./pages/Signup"
 import './App.css';
